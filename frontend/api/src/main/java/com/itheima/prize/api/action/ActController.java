c659fe4e9220e9be513031ba9748b1845240ad92
captaindeng <captaindeng12138@gmail.com>
2024-03-18 15:02:52 +0000

    init repo

---
+++ frontend/api/src/main/java/com/itheima/prize/api/action/ActController.java
@@ -0,0 +1,59 @@
+package com.itheima.prize.api.action;
+
+import com.alibaba.fastjson.JSON;
+import com.itheima.prize.api.config.LuaScript;
+import com.itheima.prize.commons.config.RabbitKeys;
+import com.itheima.prize.commons.config.RedisKeys;
+import com.itheima.prize.commons.db.entity.*;
+import com.itheima.prize.commons.db.mapper.CardGameMapper;
+import com.itheima.prize.commons.db.service.CardGameService;
+import com.itheima.prize.commons.utils.ApiResult;
+import com.itheima.prize.commons.utils.RedisUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/act")
+@Api(tags = {"抽奖模块"})
+public class ActController {
+
+    @Autowired
+    private RedisUtil redisUtil;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private LuaScript luaScript;
+
+    @GetMapping("/go/{gameid}")
+    @ApiOperation(value = "抽奖")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="gameid",value = "活动id",example = "1",required = true)
+    })
+    public ApiResult<Object> act(@PathVariable int gameid, HttpServletRequest request){
+        //TODO
+        return null;
+    }
+
+    @GetMapping("/info/{gameid}")
+    @ApiOperation(value = "缓存信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="gameid",value = "活动id",example = "1",required = true)
+    })
+    public ApiResult info(@PathVariable int gameid){
+        //TODO
+        return null;
+    }
+}

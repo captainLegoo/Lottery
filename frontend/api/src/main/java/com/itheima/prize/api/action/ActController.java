8d859b2cd0de2f57d45555d6fb279b82a8ff753b
captaindeng <captaindeng12138@gmail.com>
2024-03-23 05:31:21 +0800

    create User lottery function

--- frontend/api/src/main/java/com/itheima/prize/api/action/ActController.java
+++ frontend/api/src/main/java/com/itheima/prize/api/action/ActController.java
@@ -2,7 +2,9 @@ package com.itheima.prize.api.action;
 
 import com.itheima.prize.api.config.LuaScript;
 import com.itheima.prize.commons.config.RedisKeys;
-import com.itheima.prize.commons.db.entity.*;
+import com.itheima.prize.commons.db.entity.CardGame;
+import com.itheima.prize.commons.db.entity.CardProduct;
+import com.itheima.prize.commons.db.entity.CardUser;
 import com.itheima.prize.commons.utils.ApiResult;
 import com.itheima.prize.commons.utils.RedisUtil;
 import io.swagger.annotations.Api;
@@ -21,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 @RestController
@@ -46,8 +47,83 @@ public class ActController {
             @ApiImplicitParam(name="gameid",value = "活动id",example = "1",required = true)
     })
     public ApiResult<Object> act(@PathVariable int gameid, HttpServletRequest request){
-        //TODO
-        return null;
+        // 用户未登录
+        CardUser cardUser = (CardUser) request.getSession().getAttribute("user");
+        //if (Objects.isNull(cardGame)) {
+        //    return new ApiResult(-1, "活动不存在", null);
+        //}
+        if (Objects.isNull(cardUser)) {
+            return new ApiResult(-1, "未登录", null);
+        }
+        Integer userId = cardUser.getId();
+        Integer userLevel = cardUser.getLevel();
+        // 获取活动信息-基本信息/会员可抽奖次数/会员最大中奖次数
+        CardGame cardGame = (CardGame) redisUtil.get(RedisKeys.INFO + gameid);
+        Integer enterTimes = (Integer) redisUtil.hget(RedisKeys.MAXENTER + gameid, userLevel.toString());
+        Integer goalTimes = (Integer) redisUtil.hget(RedisKeys.MAXGOAL + gameid, userLevel.toString());
+        // 获取用户已抽奖次数
+        Integer userEnterCount = (Integer) redisUtil.get(RedisKeys.USERENTER + gameid + "_" + userId);
+        // 获取用户已中奖次数
+        Integer userGoalCount = (Integer) redisUtil.get(RedisKeys.USERHIT + gameid + "_" + userId);
+
+        // 如果用户抽奖次数为空，则初始化为 0
+        if (Objects.isNull(userEnterCount)) {
+            redisUtil.set(RedisKeys.USERENTER + gameid + "_" + userId, 0);
+            userEnterCount = 0;
+        }
+        // // 如果用户中奖次数为空，则初始化为 0
+        if (Objects.isNull(userGoalCount)) {
+            redisUtil.set(RedisKeys.USERHIT + gameid + "_" + userId, 0);
+            userGoalCount = 0;
+        }
+
+        // 获取当前时间戳
+        long currentTimeStamp = new Date().getTime();
+        // 获取开始时间戳
+        long startTimeStamp = cardGame.getStarttime().getTime();
+        // 获取结束时间戳
+        long endTimeStamp = cardGame.getEndtime().getTime();
+        // 活动未开始
+        if (startTimeStamp > currentTimeStamp) {
+            return new ApiResult(-1, "活动未开始", null);
+        }
+        // 活动已结束
+        if (endTimeStamp < currentTimeStamp) {
+            return new ApiResult(-1, "活动已结束", null);
+        }
+
+        // 您的抽奖次数已用完
+        if (enterTimes == null || userEnterCount >= enterTimes) {
+            return new ApiResult(-1, "您的抽奖次数已用完", null);
+        }
+
+        // 您已达到最大中奖数
+        if (goalTimes == null || userGoalCount >= goalTimes) {
+            return new ApiResult(-1, "您已达到最大中奖数", null);
+        }
+
+        // 记录用户参与次数
+        redisUtil.set(RedisKeys.USERENTER + gameid + "_" + userId, ++userEnterCount);
+
+        // 抽令牌
+        //Long token = luaScript.tokenCheck("game_" + gameid, String.valueOf(new Date().getTime()));
+        Long token = luaScript.tokenCheck(RedisKeys.TOKENS + gameid, String.valueOf(new Date().getTime()));
+        if (token == 0L) {
+            return new ApiResult(-1,"奖品已抽光",null);
+        } else if (token == 1L) {
+            return new ApiResult(0,"未中奖",null);
+        } else {
+            //token有效，中奖！
+            CardProduct cardProduct = (CardProduct) redisUtil.get(RedisKeys.TOKEN + gameid + "_" + token);
+            // 记录用户中奖数
+            redisUtil.set(RedisKeys.USERHIT + gameid + "_" + userId, ++userGoalCount);
+            log.info("token有效，中奖！ -> {}, {}", tokenConvertToDate(token) , cardProduct);
+
+            // TODO 异步通知中奖信息
+
+            // 返回恭喜中奖与数据
+            return new ApiResult<>(1,"恭喜中奖",cardProduct);
+        }
     }
 
     @GetMapping("/info/{gameid}")
